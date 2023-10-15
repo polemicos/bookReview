f178c282efb7594059836c47925d39c5bd349e35
polemicos <nikitarakovich61@gmail.com>
2023-10-15 22:14:19 +0200

    add review page and binding

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookConroller;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,9 @@ Route::get('/', function () {
     return redirect('/books');
 });
 
-Route::resource('books', BookConroller::class);
+Route::resource('books', BookConroller::class)
+    ->only(['index', 'show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
